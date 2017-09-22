d43030f2a3efc875d15b9feeeeb1323732be0b66
Eric <nomad255@gmail.com>
2017-09-22 11:52:47 -0500

    auth test

--- web.js
+++ web.js
@@ -3,7 +3,27 @@ var express = require('express')
   , http = require('http')
   , path = require('path');
 
+var passport = require('passport');
+var Strategy = require('passport-http').BasicStrategy;
+var db = require('./db');
 var fs = require('fs');
+
+// Configure the Basic strategy for use by Passport.
+//
+// The Basic strategy requires a `verify` function which receives the
+// credentials (`username` and `password`) contained in the request.  The
+// function must verify that the password is correct and then invoke `cb` with
+// a user object, which will be set at `req.user` in route handlers after
+// authentication.
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
 
 
 var app = express.createServer(express.logger());
@@ -17,8 +37,11 @@ app.use(express.static('public'));
 
 buf = fs.readFileSync("index.html","utf-8");
 
-app.get('/', function(request, response) {
-  response.send(buf.toString('utf8', 0, buf.length));
+app.get('/',
+ passport.authenticate('basic', { session: false }),
+ function(request, response) {
+  response.json({ username: request.user.username, email: request.user.emails[0].value });
+  //response.send(buf.toString('utf8', 0, buf.length));
 });
 
 index2 = fs.readFileSync("index2.html","utf-8");
