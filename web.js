8f1e246dc7d93583adc7dd931f2162ac8b286f7a
Eric <nomad255@gmail.com>
2017-11-06 01:12:16 -0600

    final updates

--- web.js
+++ web.js
@@ -35,19 +35,19 @@ app.use(express.static('js'));
 app.use(express.static('img'));
 app.use(express.static('public'));
 
-//buf = fs.readFileSync("index.html","utf-8");
+buf = fs.readFileSync("index.html","utf-8");
 
-//app.get('/',
-// passport.authenticate('basic', { session: false }),
-// function(request, response) {
-//  response.send(buf.toString('utf8', 0, buf.length));
-//  //response.send(buf.toString('utf8', 0, buf.length));
-//});
-
-index2 = fs.readFileSync("index2.html","utf-8");
-app.get('/', function(request, response) {
-  response.send(index2.toString('utf8', 0, index2.length));
+app.get('/',
+ passport.authenticate('basic', { session: false }),
+ function(request, response) {
+  response.send(buf.toString('utf8', 0, buf.length));
+  //response.send(buf.toString('utf8', 0, buf.length));
 });
+
+//index2 = fs.readFileSync("index2.html","utf-8");
+//app.get('/', function(request, response) {
+//  response.send(index2.toString('utf8', 0, index2.length));
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
